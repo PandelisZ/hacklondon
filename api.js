2c31d3e8a6c61852ddf3f0dd3c8a5aed2acd42ff
Hgat <hgat@hotmail.co.uk>
2016-02-27 18:07:08 +0000

    Added place.js

--- api.js
+++ api.js
@@ -1,30 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var place = require('./place.js');
 
 var port = process.env.PORT || 8000;
 
-
-router.use(function(req, res, next){
-  console.log('Something is happening');
-  next();
-});
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-
-
-
-
-
-
-
-
-//The express stuff
-app.use('/', router);
 app.listen(port, function(){
   console.log('Running on' + ' :' + port);
 });
+
+place.place(function(result) {console.log(result)} );
